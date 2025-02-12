b72dbbfd6cdb0409fd9227c4a5f7a36a32d2f2ba
yueyueqiang <2403495494@qq.com>
2025-02-12 22:04:12 +0800

    first commit

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "lovesqiang",
+  // description是SEO要用的
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { 
+        text: 'Home',
+        items: [
+          { text:'首页', link: '/' },
+          { text: 'Markdown 示例', link: '/markdown-examples' },
+          { text: 'Runtime API 示例', link: '/api-examples' }
+        ]
+
+      },
+      { text: 'Examples', link: '/markdown-examples' },
+      { text: '其他', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    footer:
+      {copyright : "Copyright@ 2025 lovesqiang"}
+    
+  }
+})
---
+++ docs/index.md
@@ -0,0 +1,25 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "lovesqiang的文档"
+  text: "A VitePress Site"
+  tagline: 持续学习，持续进步
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
